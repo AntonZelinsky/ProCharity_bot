dbc6f5722ef5ef73bc88e52698a121d08fe9e36b
PGluk <glukhov.p@gmail.com>
2021-06-18 14:08:22 +0600

    Added routes to user api

--- app/routers.py
+++ app/routers.py
@@ -1,6 +1,7 @@
 from app import api
 from app.api import Login, Register, UsersList, Refresh, User_item
 
+
 api.add_resource(UsersList, '/api/users/')
 api.add_resource(User_item, '/api/users/<string:username>/')
 api.add_resource(Register, '/api/register/')

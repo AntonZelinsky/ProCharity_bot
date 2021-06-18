3591d42304671e12a39fcee0fdedfe3376ca687d
PGluk <glukhov.p@gmail.com>
2021-06-19 02:46:37 +0600

    Registered additional endpoints

--- app/swagger.py
+++ app/swagger.py
@@ -1,8 +1,12 @@
 from app import docs
-from app.api import Refresh, Register, Login, UsersList, User_item
+from app.apis.users import UsersList, User_item
+from app.apis.auth import Refresh, Register, Login, PasswordReset
+from app.apis.messages import SendMessage
 
-docs.register(Register)
 docs.register(Login)
 docs.register(Refresh)
+docs.register(Register)
+docs.register(PasswordReset)
 docs.register(UsersList)
 docs.register(User_item)
+docs.register(SendMessage)

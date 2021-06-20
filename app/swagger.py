6a2df0a82253384dac9d91f55138ff13b62c462c
PGluk <glukhov.p@gmail.com>
2021-06-20 23:01:47 +0600

    Updated reg.list

--- app/swagger.py
+++ app/swagger.py
@@ -1,12 +1,15 @@
 from app import docs
 from app.apis.users import UsersList, User_item
-from app.apis.auth import Refresh, Register, Login, PasswordReset
-from app.apis.messages import SendMessage
+from app.apis.auth import Refresh, Login, PasswordReset
+from app.apis.registration import UserRegister, InvitationChecker
+from app.apis.messages import SendMessage, SendRegistrationInvitation
 
 docs.register(Login)
 docs.register(Refresh)
-docs.register(Register)
+docs.register(UserRegister)
 docs.register(PasswordReset)
 docs.register(UsersList)
 docs.register(User_item)
 docs.register(SendMessage)
+docs.register(SendRegistrationInvitation)
+docs.register(InvitationChecker)

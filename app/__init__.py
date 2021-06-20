b0e54cc57fbf25acbeb9835c11cbb29df8b36eed
PGluk <glukhov.p@gmail.com>
2021-06-20 22:20:13 +0600

    Added mail initialization

--- app/__init__.py
+++ app/__init__.py
@@ -9,12 +9,13 @@ from flask_cors import CORS
 from flask_restful import Api
 from flask_apispec.extension import FlaskApiSpec
 from password_validation import PasswordPolicy
-
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object(Config)
 jwt = JWTManager(app)
 api = Api(app)
+mail = Mail(app)
 password_policy = PasswordPolicy(**PASSWORD_POLICY)
 
 app.config.update(**Config.APISPEC_SPEC)

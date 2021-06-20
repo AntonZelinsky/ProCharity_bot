05e0e7e290afac209df56baa9191da40759e21a0
PGluk <glukhov.p@gmail.com>
2021-06-21 03:25:10 +0600

    added SendPushEmailMessage

--- app/apis/messages.py
+++ app/apis/messages.py
@@ -1,74 +1,32 @@
-from flask_restful import Resource, request
+from flask_restful import Resource
 from flask_apispec.views import MethodResource
 from flask import jsonify, render_template
 from flask_apispec import doc, use_kwargs
-from flask_jwt_extended import jwt_required
 from app import config
 from marshmallow import fields
 from app.messages import send_email
-import uuid
-from app.models import Register, UserAdmin
-import datetime
 from app.database import db_session
-from email_validator import validate_email, EmailNotValidError
+from app.models import User
 
 
-# TODO Complete push messages from admin panel
-class SendMessage(MethodResource, Resource):
+# TODO Optimize
+class SendPushEmailMessage(MethodResource, Resource):
     @doc(description='Send message to registered users',
          tags=['Send Message'],
-         params=config.PARAM_HEADER_AUTH
-         )
-    @use_kwargs({'message': fields.Str()})
-    @jwt_required()
-    def post(self):
-        return jsonify(message='Message has been sent to users')
 
-
-class SendRegistrationInvitation(MethodResource, Resource):
-    @doc(description='Send email with invitation url',
-         tags=['User Registration'],
-         params=config.PARAM_HEADER_AUTH
          )
-    @use_kwargs({'email': fields.Str()})
-    @jwt_required()
+    @use_kwargs({'message': fields.Str(), 'subject': fields.Str()})
     def post(self, **kwargs):
-        email = kwargs.get('email')
+        message = kwargs.get('message')
+        subject = kwargs.get('subject')
+        template = render_template(config.PUSH_TEMPLATE, message=message)
+        emails = [x.email for x in db_session.query(User).filter(User.email.isnot(None))]
 
-        # validate of an email if one included in the request.
-        if email:
-            try:
-                validate_email(email)
+        send_email(recipients=emails, subject=subject, template=template)
+        return jsonify(emails)
 
-            except EmailNotValidError as ex:
-                return jsonify(message=str(ex))
 
-        subject = config.SUBJECT
-        now = datetime.datetime.now()
-        token = str(uuid.uuid4())
-        inv_link = f'{request.host_url}/register/?inv_token:{token}'
 
-        template = render_template(config.INVITATION_TEMPLATE, inv_link=inv_link)
-
-        register_record = Register.query.filter_by(email=email).first()
-        # checks if the invitation already sent.
-        if register_record:
-            # if the invitation exist, try search the user in User Admin db
-            admin_user = UserAdmin.query.filter_by(email=email).first()
-            if admin_user:
-                return jsonify('The user already exist in the database.')
-
-            register_record.token = token
-            register_record.inv_created_date = now
-            db_session.commit()
-        else:
-            user = Register(email=email, token=token, inv_created_date=now)
-            db_session.add(user)
-            db_session.commit()
-
-        try:
-            send_email(recipients=[email], subject=subject, template=template)
-        except Exception as ex:
-            return jsonify(str(ex))
-
-        return jsonify('The email with the invitation url was been sent to specified email address')
+class SendPushTelegramMessage(MethodResource, Resource):
+  def push(self):
+      telegram_ids = [x.email for x in db_session.query(User).filter(User.email.isnot(None))]

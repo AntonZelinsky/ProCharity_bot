68c05541f5818a8d025397391df54b367a057d43
Лебедев Виктор Сергеевич <rs.porosenok@gmail.com>
2021-06-21 18:05:07 +0300

    Change custom sql script to sqlalchemy

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import os
-import sqlite3
+from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy.sql import select
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,10 +9,13 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_NAME = os.path.basename(os.getenv('DATABASE_URL'))
 DB_PATH = os.path.join(BASE_DIR, DB_NAME)
 
+engine = create_engine('sqlite:///' + DB_PATH)
+
 
 def get_category():
-    with sqlite3.connect(DB_PATH) as conn:
-        cursor = conn.cursor()
-        cursor.execute('select * from category')
-
-    return [i[2] for i in cursor]
+    with engine.connect() as conn:
+        metadata = MetaData(engine)
+        category = Table('category', metadata, autoload=True)
+        stmt = select(category.c.name)
+        res = conn.execute(stmt)
+        return [i[0] for i in res]

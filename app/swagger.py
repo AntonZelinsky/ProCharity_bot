da166402555e4e3b01a5b4eaddac05f88f105108
PGluk <glukhov.p@gmail.com>
2021-06-22 11:52:00 +0600

    added Create_categories and Create_tasks

--- app/swagger.py
+++ app/swagger.py
@@ -6,6 +6,8 @@ from app.apis.auth.password_reset import PasswordReset
 from app.apis.auth.registration import UserRegister
 from app.apis.auth.invitation_checker import InvitationChecker
 from app.apis.auth.send_reg_invitation import SendRegistrationInvitе
+from app.apis.create_categories import Create_categories
+from app.apis.create_tasks import Create_tasks
 
 docs.register(Login)
 docs.register(Refresh)
@@ -15,3 +17,6 @@ docs.register(UsersList)
 docs.register(User_item)
 docs.register(SendRegistrationInvitе)
 docs.register(InvitationChecker)
+docs.register(Create_categories)
+docs.register(Create_tasks)
+

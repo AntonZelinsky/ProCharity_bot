b14bd0e4a99c77b040df20a603105fffd8b31130
PGluk <glukhov.p@gmail.com>
2021-06-25 01:22:49 +0600

    The debug parameter was removed and the startup parameters were changed

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
 from app import app
-from multiprocessing import Process
 from bot.charity_bot import main
 
 if __name__ == "__main__":
+    main()
     app.run()
-
-    _bot = Process(target=main)
-    _bot.run()

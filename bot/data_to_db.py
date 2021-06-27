fb7ec29e1b64421218f5eff85867ed93153a9273
PGluk <glukhov.p@gmail.com>
2021-06-27 10:09:58 +0600

    optimized log_command

--- bot/data_to_db.py
+++ bot/data_to_db.py
@@ -3,6 +3,7 @@ from app.database import db_session
 from datetime import datetime
 import inspect
 
+
 def add_user(message):
     telegram_id = message.chat.id
     last_name, first_name = message.chat.last_name, message.chat.first_name
@@ -85,26 +86,31 @@ def log_command(command, start_menu=False, ignore_func=None, ):
     :param ignore_func: Ignoring logging based on the name of the called function.
     :return:
     """
+
     def log(func):
         def wrapper(*args, **kwargs):
-            current_frame = inspect.currentframe()
-            caller_frame = current_frame.f_back
-            code_obj = caller_frame.f_code
-            code_obj_name = code_obj.co_name
+
+            if ignore_func:
+                current_frame = inspect.currentframe()
+                caller_frame = current_frame.f_back
+                code_obj = caller_frame.f_code
+                code_obj_name = code_obj.co_name
+                if code_obj_name == ignore_func:
+                    return func(*args, **kwargs)
 
             if start_menu:
                 telegram_id = args[0].message.chat.id
             else:
                 telegram_id = args[0].callback_query.message.chat.id
 
-            if not ignore_func or code_obj_name != ignore_func:
-                statistic = Statistics(telegram_id=telegram_id,
-                                       command=command,
-                                       added_date=datetime.now())
+            statistic = Statistics(telegram_id=telegram_id,
+                                   command=command,
+                                   added_date=datetime.now())
 
-                db_session.add(statistic)
-                db_session.commit()
+            db_session.add(statistic)
+            db_session.commit()
             return func(*args, **kwargs)
+
         return wrapper
     return log
 

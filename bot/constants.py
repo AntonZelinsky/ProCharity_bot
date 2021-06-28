ccfb5942147fc8ce843d4f1c94c64da679879191
PGluk <glukhov.p@gmail.com>
2021-06-28 16:34:28 +0600

    created constants.py and moved log constants to the file

---
+++ bot/constants.py
@@ -0,0 +1,21 @@
+LOG_COMMANDS_NAME = {
+    'start': 'Старт',
+    'change_user_categories': 'Изменение категории',
+    'choose_category': 'Меню выбора категории',
+    'after_category_choose': 'Готово',
+    'open_menu': 'Открыть меню',
+    'show_open_task': 'Посмотреть открытые задания',
+    'send_task_to_friend': 'Переслать задание другу',
+    'ask_question': 'Задать вопрос',
+    'after_ask_question': 'Меню после вопроса',
+    'no_relevant_category': 'Моих компетенций здесь нет',
+    'email_feedback': 'Отправка емейла',
+    'add_new_category': 'Добавление новой категории',
+    'after_add_new_category': 'Меню после добавления новой категории',
+    'add_new_feature': 'Хочу новый функционал бота',
+    'after_add_new_feature': 'Меню после добавление функционала',
+    'about': 'О платформе',
+    'stop_task_subscription': 'Остановить/включить подписку на задания',
+    'cancel': 'Отмена',
+
+}

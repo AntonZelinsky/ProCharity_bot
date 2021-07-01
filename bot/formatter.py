897497517b2276f700a5b54e82cef55f71b837a7
PGluk <glukhov.p@gmail.com>
2021-07-01 21:44:18 +0600

    Updated

--- bot/formatter.py
+++ bot/formatter.py
@@ -1,8 +1,9 @@
-from datetime import datetime
+import locale
+locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
 
 def display_task(t):
     return f'<b>{t[0].title}</b>\n\n' \
-           f'От: {t[0].name_organization}, {t[0].location}\n\n' \
+           f'От {t[0].name_organization}, {t[0].location}\n\n' \
            f'Бонусы {"💎" * t[0].bonus}\n' \
            f'Категория: {t[1]}\n' \
            f'Срок: {t[0].deadline.strftime("%d %B %Y")}г.\n\n' \
@@ -11,7 +12,7 @@ def display_task(t):
 
 def display_task_notification(task):
     return (f'<b>{task.title}</b>\n\n'
-            f'От: {task.name_organization}, {task.location}\n\n'
+            f'От {task.name_organization}, {task.location}\n\n'
             f'Бонусы {"💎" * task.bonus}\n'
             f'Категория: {task.categories.name}\n'
             f'Срок: {task.deadline.strftime("%d %B %Y")}г.\n\n'

1fa71bd7361e09547e285cb5e663641dcc13aaf3
PGluk <glukhov.p@gmail.com>
2021-07-01 15:04:33 +0600

    updated TelegramNotification class

--- bot/messages.py
+++ bot/messages.py
@@ -3,7 +3,6 @@ from app.models import User
 from telegram import Bot, ParseMode, error
 from bot.charity_bot import updater, logger
 
-import datetime
 from app import config
 
 bot = Bot(config.TELEGRAM_TOKEN)
@@ -14,30 +13,37 @@ class TelegramNotification:
     This class describes the functionality for working with notifications in Telegram.
     """
 
-    def __init__(self, has_mailing='subscribed') -> None:
+    def __init__(self, has_mailing: str = 'subscribed') -> None:
         self.has_mailing = has_mailing
 
-    def send_notification(self, message):
+    # TODO refactoring https://github.com/python-telegram-bot/python-telegram-bot/wiki/Avoiding-flood-limits
+    def send_notification(self, message, query=None):
         """
            Adds queue to send notification to telegram chats.
 
         :param message: Message to add to the sending queue
+        :param query: Users query
         :return:
         """
 
-        if not self.has_mailing in ['all', 'subscribed', 'unsubscribed']:
+        if self.has_mailing not in ('all', 'subscribed', 'unsubscribed'):
             return False
 
-        telegram_chats = []
-        query = db_session.query(User.telegram_id)
+        telegram_chats = query
 
-        if self.has_mailing == 'subscribed':
+        if not telegram_chats:
+            query = db_session.query(User.telegram_id)
+
+        if (self.has_mailing == 'subscribed'
+                and not telegram_chats):
             telegram_chats = query.filter(User.has_mailing.is_(True))
 
-        if self.has_mailing == 'unsubscribed':
+        if (self.has_mailing == 'unsubscribed'
+                and not telegram_chats):
             telegram_chats = query.filter(User.has_mailing.is_(False))
 
-        if self.has_mailing == 'all':
+        if (self.has_mailing == 'all'
+                and not telegram_chats):
             telegram_chats = query
 
         chats = [user for user in telegram_chats]
@@ -46,7 +52,7 @@ class TelegramNotification:
             context = {'message': message, 'chats': part}
 
             updater.job_queue.run_once(self.__send_by_subscription_status, i, context=context,
-                                       name=f'Notification: {message.message[0:10]}')
+                                       name=f'Notification: {message[0:10]}_{i}')
 
         return True
 
@@ -54,7 +60,7 @@ class TelegramNotification:
         """
         Sends the message to all telegram users registered in the database.
 
-        :param context: A dic containing the sending parameters and the message body
+        :param context: A dict containing the sending parameters and the message body
         :return:
         """
         job = context.job
@@ -63,13 +69,9 @@ class TelegramNotification:
 
         for user in chats:
             try:
-                bot.send_message(chat_id=user.telegram_id, text=message.message, parse_mode=ParseMode.MARKDOWN)
+                bot.send_message(chat_id=user.telegram_id, text=message, parse_mode=ParseMode.MARKDOWN)
             except error.BadRequest as ex:
                 logger.error(str(ex), user.telegram_id)
-
-        message.was_sent = True
-        message.sent_date = datetime.datetime.now()
-        db_session.commit()
 
     @staticmethod
     def __split_chats(array, size):

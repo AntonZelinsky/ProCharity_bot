4a7b8df83ed9efd60b7758dd6302f9999f3fd093
musserg <s.musorin@yandex.ru>
2021-07-09 19:59:40 +0400

    the sample in analysis was made in 30 days

--- app/apis/analysis.py
+++ app/apis/analysis.py
@@ -9,12 +9,11 @@ from flask import request, jsonify, make_response
 from flask_jwt_extended import jwt_required
 
 
-
 class Analysis(MethodResource, Resource):
     @doc(description='Analysis statistics',
          tags=['Analysis']
          )
-    #@jwt_required()
+    @jwt_required()
     def get(self):
         users = db_session.query(User.has_mailing).all()
         num_users = len(users)
@@ -23,11 +22,13 @@ class Analysis(MethodResource, Resource):
             if user['has_mailing']:
                 active_users += 1
         deactivated_users = num_users - active_users
-        command_stats = db_session.query(Statistics.command,
-            func.count(Statistics.command)).group_by(Statistics.command).all()
-        reasons_canceling = db_session.query(ReasonCanceling.reason_canceling,
-            func.count(ReasonCanceling.reason_canceling)).\
-            group_by(ReasonCanceling.reason_canceling).all()
+        command_stats = db_session.query(
+            Statistics.command, func.count(Statistics.command)
+        ).group_by(Statistics.command).all()
+        reasons_canceling = db_session.query(
+            ReasonCanceling.reason_canceling,
+            func.count(ReasonCanceling.reason_canceling)
+        ).group_by(ReasonCanceling.reason_canceling).all()
         return make_response(jsonify(added_users=users_created_date(),
                                      active_users=active_users,
                                      deactivated_users=deactivated_users,
@@ -39,13 +40,16 @@ def users_created_date():
     today = datetime.now().date()
     date_begin = today - timedelta(days=30)
     added_users = dict(
-        db_session.query(func.to_char(User.date_registration, 'YYYY-MM-DD'),
-        func.count(User.date_registration)).
-        filter(User.date_registration > date_begin).
-        group_by(func.to_char(User.date_registration, 'YYYY-MM-DD')).all()
+        db_session.query(
+            func.to_char(User.date_registration, 'YYYY-MM-DD'),
+            func.count(User.date_registration)
+        ).filter(User.date_registration > date_begin).group_by(
+            func.to_char(User.date_registration, 'YYYY-MM-DD')
+        ).all()
     )
     return {
         (date_begin + timedelta(days=n)).strftime('%Y-%m-%d'):
-        added_users.get(( date_begin + timedelta(days=n)).
-        strftime('%Y-%m-%d'), 0) for n in range(1, 31)
+        added_users.get((date_begin + timedelta(days=n)).strftime(
+            '%Y-%m-%d'
+        ), 0) for n in range(1, 31)
     }

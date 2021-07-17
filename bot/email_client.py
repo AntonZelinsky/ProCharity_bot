b2ca235c4b4a89f8acad48c0415675e410a8dad3
musserg <s.musorin@yandex.ru>
2021-07-17 18:42:06 +0400

    added the function of sending mail to procharrity

--- bot/email_client.py
+++ bot/email_client.py
@@ -1,15 +1,10 @@
 import os
 
-from dotenv import load_dotenv
 from flask import render_template
 from flask_mail import Message
 
 from app import app, config, mail
 from app.models import User
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-dotenv_path = os.path.join(BASE_DIR, '.env')
-load_dotenv(dotenv_path)
 
 
 def send_email(telegram_id, message, subject):
@@ -19,17 +14,18 @@ def send_email(telegram_id, message, subject):
     name = f'{user.last_name} {user.first_name}'
     id = user.external_id
     with app.app_context():
-        email_message = render_template(
+        template = render_template(
             config.PROCHARRITY_TEMPLATE,
             message=message,
             id=id,
+            telegram_id=telegram_id,
             email=email,
             name=name
         )
         msg = Message(
             subject=subject,
             recipients=recipients,
-            html=email_message
+            html=template
         )
         mail.send(msg)
     return

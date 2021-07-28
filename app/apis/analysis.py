9a88d3a56a663b081b9aed0a003b1a0e9425b840
Viktoria <viktrols@yandex.ru>
2021-07-28 21:18:26 +0300

    refactoring app/apis/analysis.py

--- app/apis/analysis.py
+++ app/apis/analysis.py
@@ -1,12 +1,14 @@
-from app.models import ReasonCanceling, User, Statistics
-from app.database import db_session
-from datetime import datetime, timedelta
-from flask_apispec.views import MethodResource
+from flask import jsonify, make_response
 from flask_apispec import doc
+from flask_apispec.views import MethodResource
+from flask_jwt_extended import jwt_required
 from flask_restful import Resource
 from sqlalchemy.sql import func
-from flask import jsonify, make_response
-from flask_jwt_extended import jwt_required
+
+from app.models import ReasonCanceling, Statistics, User
+from app.database import db_session
+from datetime import datetime, timedelta
+
 from bot.constants import REASONS
 
 
@@ -16,13 +18,10 @@ class Analysis(MethodResource, Resource):
          )
     @jwt_required()
     def get(self):
-        users = db_session.query(User.has_mailing).all()
+        users = (db_session.query(User.has_mailing).all())
         num_users = len(users)
-        active_users = 0
-        for user in users:
-            if user['has_mailing']:
-                active_users += 1
-        deactivated_users = num_users - active_users
+        subscribed_users = len([user for user in users if user['has_mailing']])
+        not_subscribed_users = num_users - subscribed_users
         command_stats = db_session.query(
             Statistics.command, func.count(Statistics.command)
         ).group_by(Statistics.command).all()
@@ -32,11 +31,11 @@ class Analysis(MethodResource, Resource):
         ).group_by(ReasonCanceling.reason_canceling).all()
         reasons_canceling = {
             REASONS.get(key, 'Другое'):
-            value for key, value in reasons_canceling_from_db
+                value for key, value in reasons_canceling_from_db
         }
         return make_response(jsonify(added_users=users_created_date(),
-                                     active_users=active_users,
-                                     deactivated_users=deactivated_users,
+                                     subscribed_users=subscribed_users,
+                                     not_subscribed_users=not_subscribed_users,
                                      command_stats=dict(command_stats),
                                      reasons_canceling=dict(reasons_canceling)), 200)
 
@@ -54,7 +53,7 @@ def users_created_date():
     )
     return {
         (date_begin + timedelta(days=n)).strftime('%Y-%m-%d'):
-        added_users.get((date_begin + timedelta(days=n)).strftime(
-            '%Y-%m-%d'
-        ), 0) for n in range(1, 31)
+            added_users.get((date_begin + timedelta(days=n)).strftime(
+                '%Y-%m-%d'
+            ), 0) for n in range(1, 31)
     }

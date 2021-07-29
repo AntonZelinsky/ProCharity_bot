241ce286750acd8f0bdbe5f698358ce5b0f493d8
Viktoria <viktrols@yandex.ru>
2021-07-29 14:36:29 +0300

    fix analytics.py

--- app/apis/analytics.py
+++ app/apis/analytics.py
@@ -16,7 +16,7 @@ class Analytics(MethodResource, Resource):
     @doc(description='Analytics statistics',
          tags=['Analytics']
          )
-    #@jwt_required()
+    @jwt_required()
     def get(self):
         users = db_session.query(User.has_mailing).all()
         number_users = len(users)

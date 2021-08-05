eccd121fb874735a5e1a0ab596c3cb3bdd5a177b
Viktoria <viktrols@yandex.ru>
2021-08-05 15:16:50 +0300

    Rename send_reg_invitation to send_registration_invite + refactoring

--- app/swagger.py
+++ app/swagger.py
@@ -6,7 +6,7 @@ from app.apis.auth.password_reset import PasswordReset
 from app.apis.auth.registration import UserRegister
 from app.apis.auth.external_users_registration import ExternalUserRegistration
 from app.apis.auth.invitation_checker import InvitationChecker
-from app.apis.auth.send_reg_invitation import SendRegistrationInvite
+from app.apis.auth.send_registration_invite import SendRegistrationInvite
 from app.apis.messages import SendTelegramNotification
 from app.apis.categories import CreateCategories
 from app.apis.tasks import CreateTasks

e9b6c1104c220fda1595df2d91a95d34bb9e73a2
Viktoria <viktrols@yandex.ru>
2021-09-21 16:25:57 +0300

    check config.TOKEN_FOR_WEBHOOKS is not None

--- app/apis/check_webhooks_token.py
+++ app/apis/check_webhooks_token.py
@@ -5,19 +5,19 @@ from functools import wraps
 from app import config
 from app.logger import webhooks_logger as logger
 
-WEBHOOKS_TOKEN = hashlib.sha1(config.TOKEN_FOR_WEBHOOKS.encode('utf-8')).hexdigest()
-
 
 def check_webhooks_token(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         try:
-            webhooks_token = request.headers['token']
+            request.headers['token']
         except KeyError:
             logger.info('Request without token!')
             return make_response(jsonify(result='Access is denied'), 403)
-        token = hashlib.sha1(webhooks_token.encode('utf-8')).hexdigest()
-        if not token or token != WEBHOOKS_TOKEN:
+        token = request.headers['token']
+        if not config.TOKEN_FOR_WEBHOOKS:
+            return f(*args, **kwargs)
+        if not token or token != config.TOKEN_FOR_WEBHOOKS:
             logger.info('Token is invalid!')
             return make_response(jsonify(result='Access is denied'), 403)
         return f(*args, **kwargs)

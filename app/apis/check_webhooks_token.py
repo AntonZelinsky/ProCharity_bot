3e7a6d18f093aac35ca9df9c8bf58253ef15f9bf
Viktoria <viktrols@yandex.ru>
2021-09-21 17:01:34 +0300

    fix decorator

--- app/apis/check_webhooks_token.py
+++ app/apis/check_webhooks_token.py
@@ -9,15 +9,13 @@ from app.logger import webhooks_logger as logger
 def check_webhooks_token(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        try:
-            request.headers['token']
-        except KeyError:
+        if not config.TOKEN_FOR_WEBHOOKS:
+            return f(*args, **kwargs)
+        if not request.headers.get('token'):
             logger.info('Request without token!')
             return make_response(jsonify(result='Access is denied'), 403)
         token = request.headers['token']
-        if not config.TOKEN_FOR_WEBHOOKS:
-            return f(*args, **kwargs)
-        if not token or token != config.TOKEN_FOR_WEBHOOKS:
+        if token != config.TOKEN_FOR_WEBHOOKS:
             logger.info('Token is invalid!')
             return make_response(jsonify(result='Access is denied'), 403)
         return f(*args, **kwargs)

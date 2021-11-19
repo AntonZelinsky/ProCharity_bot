8e16cfa8b3e932ff42e48572d63a56cf600e21c4
Viktoria <viktrols@yandex.ru>
2021-11-19 15:43:25 +0200

    add check_file_name method

--- app/front/__init__.py
+++ app/front/__init__.py
@@ -16,4 +16,4 @@ front_api.add_resource(send_tg_notification.SendTelegramNotification,
 front_api.add_resource(users.UsersList, '/api/v1/users/')
 front_api.add_resource(users.UserItem, '/api/v1/users/<int:telegram_id>/')
 front_api.add_resource(download_log_files.DownloadLogs, '/api/v1/download_logs/')
-front_api.add_resource(download_log_files.GetListLogFiles, '/api/v1/logs/') # TODO: название эндпойнта
+front_api.add_resource(download_log_files.GetListLogFiles, '/api/v1/logs/')
--- app/front/download_log_files.py
+++ app/front/download_log_files.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from flask import send_file, request, jsonify, make_response
 from flask_apispec import doc
@@ -12,7 +13,7 @@ from app.logger import app_logger as logger
 
 class DownloadLogs(MethodResource, Resource):
     @doc(description='Download logs',
-         tags=['Download log files'],
+         tags=['Logs'],
          params={
              'log_file': {
                  'description': 'app_logs, bot_logs or webhook_logs',
@@ -22,7 +23,7 @@ class DownloadLogs(MethodResource, Resource):
              'Authorization': config.PARAM_HEADER_AUTH})     
     @jwt_required()
     def get(self):
-        log_file = request.args.get('log_file')
+        log_file = self.__check_file_name(request.args.get('log_file'))
         directory = f"../logs/{log_file}"
         try:
             return send_file(directory, as_attachment=True)
@@ -30,13 +31,18 @@ class DownloadLogs(MethodResource, Resource):
             logger.info(f'Download log files: incorrect file_name "{log_file}"')
             return 'Make sure you pass in the correct log_file'
 
+    def __check_file_name(self, file_name):
+        if file_name in re.findall(r'(?:app_logs|bot_logs|webhooks_logs)', file_name):
+            return file_name
+        return None
 
-class GetListLogFiles(MethodResource, Resource):        # TODO: название, описание
-    @doc(description='.............', 
-         tags=['.............'],
+
+class GetListLogFiles(MethodResource, Resource):
+    @doc(description='Get list of log files', 
+         tags=['Logs'],
          params={
              'Authorization': config.PARAM_HEADER_AUTH})     
     @jwt_required()
     def get(self):
-        all_log_files = os.listdir(path='./logs')
-        return make_response(jsonify(all_log_files=all_log_files), 200)
+        log_files = os.listdir(path='./logs')
+        return make_response(jsonify(log_files=log_files), 200)

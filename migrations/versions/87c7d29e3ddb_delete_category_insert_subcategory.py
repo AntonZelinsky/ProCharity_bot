e3c4d789e1ac2c96290c3800e4e94ef889b1e954
Илья Борецкий <boretskiy38@gmail.com>
2022-10-23 12:53:34 +0300

    Fix init db bug

--- migrations/versions/87c7d29e3ddb_delete_category_insert_subcategory.py
+++ migrations/versions/87c7d29e3ddb_delete_category_insert_subcategory.py
@@ -6,7 +6,7 @@ Create Date: 2022-10-22 13:27:10.144666
 
 """
 from sqlalchemy import select, insert, and_
-
+from sqlalchemy.exc import ProgrammingError, InternalError
 from app.database import db_session
 from app.models import Users_Categories, Category
 
@@ -19,16 +19,21 @@ depends_on = None
 
 
 def get_user_categories():
-    return db_session.scalars(
-        select(Users_Categories).where(
-            Users_Categories.category_id != None # noqa
-        )
-    ).all()
+    try:
+        users_categories = db_session.scalars(
+            select(Users_Categories).where(
+                Users_Categories.category_id != None # noqa
+            )
+        ).all()
+        delete_categories_insert_subcategories(users_categories)
+    except ProgrammingError:
+        pass
+    except InternalError:
+        pass
 
 
-def upgrade():
-    user_categories = get_user_categories()
-    for user_category in user_categories:
+def delete_categories_insert_subcategories(users_categories):
+    for user_category in users_categories:
         main_category_id = db_session.scalars(
             select(Category.id).where(
                 Category.id == user_category.category_id, and_(
@@ -63,6 +68,9 @@ def upgrade():
                     )
                 )
     db_session.commit()
+
+def upgrade():
+    get_user_categories()
 
 
 def downgrade():

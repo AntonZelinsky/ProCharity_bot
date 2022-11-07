e105988d3bae7ed66f6c593f3cd1de4bfcc31e4d
Илья Борецкий <boretskiy38@gmail.com>
2022-11-07 07:16:49 +0300

    Delete one function

--- migrations/versions/87c7d29e3ddb_delete_category_insert_subcategory.py
+++ migrations/versions/87c7d29e3ddb_delete_category_insert_subcategory.py
@@ -1,7 +1,7 @@
 """delete_category_insert_subcategory
 
 Revision ID: 87c7d29e3ddb
-Revises: 35477a4b423e
+Revises: 52647dd43d55
 Create Date: 2022-10-22 13:27:10.144666
 
 """
@@ -12,36 +12,23 @@ from app.models import Users_Categories, Category
 
 # revision identifiers, used by Alembic.
 revision = '87c7d29e3ddb'
-down_revision = '35477a4b423e'
+down_revision = '52647dd43d55'
 branch_labels = None
 depends_on = None
 
 
-def delete_main_category():
-    users_categories = db_session.scalars(
-        select(Users_Categories).join(Category).where(
-            Category.parent_id == None
-        )
-    ).all()
-    for user_categories in users_categories:
-        db_session.delete(user_categories)
-    db_session.commit()
-
-
-def subscribe_to_subcategory():
+def upgrade():
     categories = set(db_session.scalars(
-        select(Category).join(Users_Categories)
+        select(Category).join(Users_Categories).where(
+            Category.children != None
+        )
     ).all())
     for category in categories:
-        if category.children:
-            for user in category.users:
-                subcategories = [Users_Categories(telegram_id=user.telegram_id, category_id=children.id) for children in category.children if children not in user.categories]
-                db_session.add_all(subcategories)
-
-
-def upgrade():
-    subscribe_to_subcategory()
-    delete_main_category()
+        for user in category.users:
+            subcategories = [Users_Categories(telegram_id=user.telegram_id, category_id=children.id) for children in category.children if children not in user.categories]
+            db_session.add_all(subcategories)
+            category.users = []
+    db_session.commit()
 
 
 def downgrade():

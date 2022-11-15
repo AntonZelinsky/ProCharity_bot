b08200d4059bfd66a7cb8d413590eadf29a7f7a2
Лебедев Виктор Сергеевич <vslebedev@vtb.ru>
2022-11-15 13:23:45 +0300

    Fix category update migration

--- migrations/versions/87c7d29e3ddb_delete_category_insert_subcategory.py
+++ migrations/versions/87c7d29e3ddb_delete_category_insert_subcategory.py
@@ -18,11 +18,12 @@ depends_on = None
 
 
 def upgrade():
-    categories = set(db_session.scalars(
-        select(Category).join(Users_Categories).where(
-            Category.children != None
-        )
-    ).all())
+    categories = set(
+        db_session.execute(
+            select(Category).join(Users_Categories).where(Category.children is not None)
+        ).scalars()
+    )
+
     for category in categories:
         for user in category.users:
             subcategories = [Users_Categories(telegram_id=user.telegram_id, category_id=children.id) for children in category.children if children not in user.categories]

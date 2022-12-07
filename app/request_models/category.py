8e2584058538f348a265dc4c511a139e9b1ad175
Анна Симанова <ani.simanova@yandex.ru>
2022-12-07 15:26:45 +0200

    Pydantic-class validation for Categories

--- app/request_models/category.py
+++ app/request_models/category.py
@@ -1,6 +1,5 @@
 from typing import Optional
-from pydantic import Field, NonNegativeInt, StrictStr
-from flask_pydantic import validate
+from pydantic import Field, NonNegativeInt, StrictStr, root_validator
 
 from app.request_models.request_base import RequestBase
 
@@ -10,8 +9,8 @@ class CategoryCreateRequest(RequestBase):
     name: StrictStr = Field(min_length=2, max_length=100)
     parent_id: Optional[NonNegativeInt]
 
-    @validate('parent_id')
-    def check_not_self_parent(cls, parent_id, values):
-        if parent_id and 'id' in values and parent_id == values['id']:
+    @root_validator
+    def check_not_self_parent(cls, values):
+        if values['id'] == values['parent_id']:
             raise ValueError('category cannot inherit from itself')
-        return parent_id
+        return values
--- app/webhooks/categories.py
+++ app/webhooks/categories.py
@@ -1,13 +1,18 @@
-from flask import request, jsonify, make_response
+from flask import request, jsonify, make_response, Response
+from http import HTTPStatus
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import load_only
-from app.models import Category
-from app.database import db_session
 from flask_restful import Resource
 from flask_apispec.views import MethodResource
 from flask_apispec import doc
+from pydantic import parse_obj_as, ValidationError
+from werkzeug.exceptions import HTTPException
 
+
+from app.database import db_session
 from app.logger import webhooks_logger as logger
+from app.models import Category
+from app.request_models.category import CategoryCreateRequest
 from app.webhooks.check_webhooks_token import check_webhooks_token
 
 
@@ -22,15 +27,12 @@ class CreateCategories(MethodResource, Resource):
              'required': True
          }})
     def post(self):
-        if not request.json:
-            logger.error('Categories: The request has no data in passed json.')
-            return make_response(jsonify(result='Json contains no data '), 400)
+        categories = request_to_context(CategoryCreateRequest, request)
 
-        categories = request.json
-        categories_dict = {int(category['id']): category for category in categories}
+        categories_dict = {int(category.id): category for category in categories}
         categories_db = Category.query.options(load_only('archive')).all()
 
-        category_id_json = [int(member['id']) for member in categories]
+        category_id_json = [int(member.id) for member in categories]
         category_id_db = [member.id for member in categories_db]
 
         category_id_db_not_archive = [member.id for member in categories_db if member.archive == False]
@@ -41,12 +43,12 @@ class CreateCategories(MethodResource, Resource):
         category_for_archive = list(set(category_id_db_not_archive) - set(category_id_json))
 
         for category in categories:
-            if int(category['id']) in category_for_adding_db:
+            if int(category.id) in category_for_adding_db:
                 c = Category(
-                    id=category['id'],
-                    name=category['name'],
+                    id=category.id,
+                    name=category.name,
                     archive=False,
-                    parent_id=category['parent_id']
+                    parent_id=category.parent_id
                 )
                 db_session.add(c)
 
@@ -94,3 +96,16 @@ class CreateCategories(MethodResource, Resource):
         category.name = category_from_dict['name']
         category.parent_id = category_from_dict['parent_id']
         category.archive = False
+
+
+def request_to_context(context, request):
+    if not request.json:
+        logger.error(f'{context}: Json contains no data')
+        raise HTTPException(f'Json contains no data, {HTTPStatus.BAD_REQUEST}')
+    try:
+        request_data = parse_obj_as(list[context], obj=request.json)
+        return request_data
+    except ValidationError as error:
+        logger.error(error)
+        raise HTTPException(f'ValidationError: {error}, {HTTPStatus.BAD_REQUEST}') from error
+ 

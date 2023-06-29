492506586e039ffe88fe93a0fcd71ed5750a5dc9
desm <desm80@yandex.ru>
2023-06-29 15:46:11 +0300

    add docstrings to InvalidAPIUsage class and handler.

--- app/error_handlers.py
+++ app/error_handlers.py
@@ -1,4 +1,9 @@
 class InvalidAPIUsage(Exception):
+    """
+    Special class for custom Exceptions raised where it is necessary.
+    If Exception is not explicitly defined raises HTTP 400 Bad Request.
+    Error Message is required to clarify erroneous program behavior.
+    """
     status_code = 400
 
     def __init__(self, message, status_code=None):
@@ -9,4 +14,7 @@ class InvalidAPIUsage(Exception):
 
 
 def invalid_api_usage(error):
+    """
+    InvalidAPIUsage handler function.
+    """
     return error.message, error.status_code
